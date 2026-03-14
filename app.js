cb66416a98449b04f56329d5060233c4e248c455
drobler2019 <drobler2019@gmail.com>
2026-03-14 12:39:48 -0500

    Agregado template y construido cartones de jugador y cpu

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hola mundo");
+(() => {
+  const templateNumero = document.querySelector("#template-numero-carton");
+  const template = document.querySelector("#template-carton");
+  const { firstElementChild: jugador } = template.content.cloneNode(true);
+  const { firstElementChild: ordenador } = template.content.cloneNode(true);
+
+  construirCartones("player", jugador, templateNumero);
+  construirCartones("cpu", ordenador, templateNumero);
+  document.body.append(jugador, ordenador);
+})();
+
+function construirCartones(nombre, elemento, templateNumero) {
+  const titulo = elemento.querySelector("h2");
+  titulo.textContent = nombre.toUpperCase();
+  for (let e = 0; e < 15; e++) {
+    const { firstElementChild: div } = templateNumero.content.cloneNode(true);
+    div.firstElementChild.textContent = e + 1;
+    elemento.lastElementChild.append(div);
+  }
+}
