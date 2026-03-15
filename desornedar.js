7cb7711f8d78f46bbb020ca8502f6be64e659e60
drobler2019 <drobler2019@gmail.com>
2026-03-14 22:42:36 -0500

    Agregado clase que contiene algoritmo de desorden de arreglos

---
+++ desornedar.js
@@ -0,0 +1,13 @@
+export function shuffle(array) {
+  let m = array.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
